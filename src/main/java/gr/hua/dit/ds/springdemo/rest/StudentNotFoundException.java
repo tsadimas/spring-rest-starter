1d239b6d67f94b54a6da3e4e8fb3da9362dab475
tsadimas <tsadimas@hua.gr>
2018-12-04 18:28:42 +0200

    added exception handling for studentcontroller

---
+++ src/main/java/gr/hua/dit/ds/springdemo/rest/StudentNotFoundException.java
@@ -0,0 +1,17 @@
+package gr.hua.dit.ds.springdemo.rest;
+
+public class StudentNotFoundException extends RuntimeException {
+
+	public StudentNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public StudentNotFoundException(String message) {
+		super(message);
+	}
+
+	public StudentNotFoundException(Throwable cause) {
+		super(cause);
+	}
+
+}
